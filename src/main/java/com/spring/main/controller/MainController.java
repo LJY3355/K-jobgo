d42b56347481e303cde8d680251a26d2d13d8f04
LJY3355 <jylee6362@gmail.com>
2025-04-30 20:44:37 +0900

    fix: add controller and config packages

---
+++ src/main/java/com/spring/main/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.spring.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String main() {
+		return "main";
+
+	}
+
+	@GetMapping("/company")
+	public String company() {
+		return "company";
+
+	}
+}
